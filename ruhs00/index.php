3581185721695716201f74a06dc61ac732c325b5
Glen Wasserfall <glen@wasserfalls.co.za>
2019-10-12 15:23:08 +0200

    ex03

---
+++ ruhs00/index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Shoppe</title>
+
+    <style>
+
+    
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <div class="logo"></div>
+            <div class="links">
+                <ul>
+                    <li>Login</li>
+                    <li>Sign Up</li>
+                </ul>
+            </div>
+        </div>
+        <div class="sub-header">
+            <ul class="categories">
+                <li>small</li>
+                <li>big</li>
+                <li>dark</li>
+                <li>color</li>
+            </ul>
+        </div>
+        <div class="content">
+            <div class="jumbo">
+                <img src="https://www.itppackaging.com/media/1147/box.jpg" alt="">
+                <div class="aside"></div>
+            </div>
+        </div>
+        <div class="footer">
+            <div class="logo-small"></div>
+            <div class="links">
+                terms of service, about, help, something else?
+                social media links
+            </div>
+        </div>
+    </div>
+</body>
+</html>
