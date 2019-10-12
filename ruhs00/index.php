7dbc1e7e90ea9eb5ff84ee89bb694497b84b5761
Matthew GERBER <h.matthew.gerber@gmail.com>
2019-10-12 16:04:41 +0200

    not centering

--- ruhs00/index.php
+++ ruhs00/index.php
@@ -5,6 +5,12 @@
 <link rel="stylesheet" type="text/css" href="./styles/style_landing.css" media="screen" />
 </head>
 <body>
-<div class="container"><div class="jumbo"><div class="logo">Dunder<br>Agon</div></div></div>
+<div class="container">
+	<div class="jumbo">
+		<div class="logo">Dunder<br>Agon</div>
+		<button class="login">login</button>
+	</div>
+	<div class="footer">csx</div>
+</div>
 </body>
 </html>
