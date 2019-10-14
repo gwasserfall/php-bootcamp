9fc2468fb244e90bc7b875538148665d5617ec0a
Glen Wasserfall <glen@wasserfalls.co.za>
2019-10-14 23:00:12 +0200

    maybe

---
+++ ruhs00/install.php
@@ -0,0 +1,28 @@
+<?php
+
+$conn = mysqli_connect("127.0.0.1", "database", "password");
+
+if (mysqli_query($conn, "CREATE DATABASE minishop")) {
+	echo "Database created successfully\n";
+	$sql = file_get_contents('minishop.sql');
+
+	$mysqli = new mysqli("127.0.0.1", "database", "password", "minishop");
+	if (mysqli_connect_errno()) { /* check connection */
+		printf("Connect failed: %s\n", mysqli_connect_error());
+	exit();
+	}
+
+	/* execute multi query */
+	if ($mysqli->multi_query($sql)) {
+		echo "Import Successful\n";
+	} else {
+		echo "Import Failed\n";
+	}
+
+ } else {
+	echo "Error creating database: " . mysqli_error($conn) . "\n";
+ }
+
+ mysqli_close($conn);
+
+?>
--- ruhs00/minishop.sql
+++ ruhs00/minishop.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Oct 14, 2019 at 11:13 AM
+-- Generation Time: Oct 14, 2019 at 01:50 PM
 -- Server version: 8.0.17
 -- PHP Version: 7.3.10
 
@@ -40,37 +40,7 @@ CREATE TABLE `basket` (
 --
 
 INSERT INTO `basket` (`id`, `user`, `product_id`, `amount`) VALUES
-(1, 'asd', 27, 1),
-(2, 'asd', 27, 1),
-(3, 'asd', 27, 1),
-(4, 'asd', 27, 1),
-(5, 'asd', 27, 1),
-(6, 'asd', 27, 1),
-(7, 'asd', 27, 1),
-(8, 'asd', 27, 1),
-(9, 'asd', 27, 1),
-(10, 'asd', 27, 1),
-(11, 'asd', 28, 1),
-(12, 'asd', 28, 1),
-(13, 'asd', 28, 1),
-(14, 'asd', 28, 1),
-(15, 'asd', 28, 1),
-(16, 'asd', 28, 1),
-(17, 'asd', 28, 1),
-(18, 'asd', 26, 1),
-(19, 'asd', 26, 1),
-(20, 'asd', 26, 1),
-(21, 'asd', 26, 1),
-(22, 'asd', 26, 1),
-(23, 'asd', 26, 1),
-(24, 'asd', 26, 1),
-(25, 'asd', 27, 1),
-(26, 'asd', 27, 1),
-(27, 'asd', 27, 1),
-(28, 'asd', 27, 1),
-(29, 'asd', 27, 1),
-(30, 'asd', 27, 1),
-(31, 'asd', 27, 1);
+(35, 'uqb8299uiib5vugge9rram4mo9', 26, 123);
 
 -- --------------------------------------------------------
 
@@ -94,34 +64,6 @@ INSERT INTO `categories` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `myguests`
---
-
-CREATE TABLE `myguests` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `firstname` varchar(30) NOT NULL,
-  `lastname` varchar(30) NOT NULL,
-  `email` varchar(50) DEFAULT NULL,
-  `reg_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `myguests1`
---
-
-CREATE TABLE `myguests1` (
-  `id` int(6) UNSIGNED NOT NULL,
-  `firstname` varchar(30) NOT NULL,
-  `lastname` varchar(30) NOT NULL,
-  `email` longblob,
-  `reg_date` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `products`
 --
 
@@ -141,9 +83,9 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`id`, `name`, `price`, `color`, `cat_id`, `img`, `reg_date`, `image`) VALUES
-(26, 'A4 Green', 1.5, 'green', 2, NULL, '2019-10-14 15:56:53', 'paper-product-alpha.png'),
-(27, 'A4 Pink', 1.5, 'pink', 2, NULL, '2019-10-14 16:40:19', 'paper-product-alpha.png'),
-(28, 'A4 Pink', 1.5, 'pink', 2, NULL, '2019-10-14 16:40:22', 'paper-product-alpha.png');
+(26, 'A4 Green', 1.5, 'green', 1, NULL, '2019-10-14 15:56:53', 'paper-product-alpha.png'),
+(27, 'A4 Pink', 1.5, 'pink', 1, NULL, '2019-10-14 16:40:19', 'paper-product-alpha.png'),
+(29, 'A5 Yellow', 2.5, 'yellow', 1, NULL, '2019-10-14 20:26:50', 'paper-product-alpha.png');
 
 -- --------------------------------------------------------
 
@@ -164,10 +106,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `login`, `email`, `hash`, `group`) VALUES
-(1, 'asdasd', 'asdas', 'aeae379a6e857728e44164267fdb7a0e27b205d757cc19899586c89dbb221930f1813d02ff93a661859bc17065eac4d6edf3c38a034e6283a84754d52917e5b0', 'user'),
-(2, 'asdasd', 'asdas', 'aeae379a6e857728e44164267fdb7a0e27b205d757cc19899586c89dbb221930f1813d02ff93a661859bc17065eac4d6edf3c38a034e6283a84754d52917e5b0', 'user'),
-(3, 'gwasserfall', '', 'da0c4124eead2e2827a8633aaac10ca15429eac1aaaac32e806d96bb2bdbe26aa13e8b09637a8af476f3ba866f8247b5786697f727b5fbeef043e4a87fb4801e', 'user'),
-(4, 'asd', '', 'e54ee7e285fbb0275279143abc4c554e5314e7b417ecac83a5984a964facbaad68866a2841c3e83ddf125a2985566261c4014f9f960ec60253aebcda9513a9b4', 'admin');
+(4, 'admin', '', 'e54ee7e285fbb0275279143abc4c554e5314e7b417ecac83a5984a964facbaad68866a2841c3e83ddf125a2985566261c4014f9f960ec60253aebcda9513a9b4', 'admin');
 
 --
 -- Indexes for dumped tables
@@ -183,18 +122,6 @@ ALTER TABLE `basket`
 -- Indexes for table `categories`
 --
 ALTER TABLE `categories`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `myguests`
---
-ALTER TABLE `myguests`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `myguests1`
---
-ALTER TABLE `myguests1`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -217,31 +144,19 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `basket`
 --
 ALTER TABLE `basket`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `id` int(18) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `myguests`
---
-ALTER TABLE `myguests`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `myguests1`
---
-ALTER TABLE `myguests1`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(18) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT for table `users`
